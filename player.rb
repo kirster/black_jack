d3647078d833add1058821ca93c07c1ee7634650
kirster <kir.sazonov@gmail.com>
2017-10-28 13:57:20 +0300

    add getters and validation method  to Player class

--- player.rb
+++ player.rb
@@ -1,7 +1,16 @@
 class Player
+  attr_reader :name, :bank, :hand
+
   def initialize(name)
     @name = name
     @bank = 100
     @hand = []
+    valiadate!
+  end
+
+  protected
+
+  def valiadate!
+    raise "Name can`be nill or empty" if name.nil? || name.empty?  
   end
 end
